f00c7afde817b64cafd0f213ea9d47b372dba61a
Rajdip Mondal <microsoftrajdip@gmail.com>
2021-09-22 01:18:13 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,12 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
 	host: "/",
-	port: "3001"
+	port: "443"
 });
+var getUserMedia = 
+navigator.getUserMedia || 
+navigator.webkitGetUserMedia || 
+navigator.mozGetUserMedia;
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 const peers = {};
